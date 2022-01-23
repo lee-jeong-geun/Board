b6770a724e7566db7a32b61571b4b9479ad44aea
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-23 20:27:44 +0900

    User entity 추가

---
+++ src/main/java/org/board/springboot/user/domain/User.java
@@ -0,0 +1,33 @@
+package org.board.springboot.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
