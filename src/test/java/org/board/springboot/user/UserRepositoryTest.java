70a8bf9e3708c7f3174b8c17f9d09dca731caa5b
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-31 17:38:39 +0900

    유저 저장 테스트 추가

--- src/test/java/org/board/springboot/user/UserRepositoryTest.java
+++ src/test/java/org/board/springboot/user/UserRepositoryTest.java
@@ -1,12 +1,40 @@
 package org.board.springboot.user;
 
+import org.board.springboot.user.domain.User;
+import org.board.springboot.user.domain.UserRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTest {
 
+    @Autowired
+    UserRepository userRepository;
 
+    @Test
+    public void 유저저장_성공() {
+        //given
+        String name = "jk";
+        String email = "jk@jk.com";
+        String password = "jkjk";
+
+        //when
+        userRepository.save(User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build());
+
+        //then
+        User user = userRepository.findAll().get(0);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getPassword()).isEqualTo(password);
+    }
 }
