1657bfe37fb6c0aff99ae716baf4f607d6c9067a
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-02 17:24:05 +0900

    UserRepository 추가

---
+++ src/main/java/org/board/springboot/user/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package org.board.springboot.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
