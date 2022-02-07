38e6f104ff336de5fd9cc92dce9fdffde2873238
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-08 00:50:37 +0900

    유저 저장 추가

--- src/main/java/org/board/springboot/user/controller/UserApiController.java
+++ src/main/java/org/board/springboot/user/controller/UserApiController.java
@@ -1,6 +1,7 @@
 package org.board.springboot.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.board.springboot.user.dto.UserSaveRequestDto;
 import org.board.springboot.user.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,4 +11,8 @@ public class UserApiController {
 
     private final UserService userService;
 
+    @PostMapping("/api/v1/users")
+    public Long save(@RequestBody UserSaveRequestDto requestDto) {
+        return userService.save(requestDto);
+    }
 }
