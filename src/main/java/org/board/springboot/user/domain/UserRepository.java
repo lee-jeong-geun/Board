0cbbefc85bbd3181dce530efcb95a8736cc5139e
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-14 23:55:04 +0900

    이메일, 비밀번호 조회 추가

--- src/main/java/org/board/springboot/user/domain/UserRepository.java
+++ src/main/java/org/board/springboot/user/domain/UserRepository.java
@@ -2,6 +2,9 @@ package org.board.springboot.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
