02d24096a24a0d92d469d39b78df34f0d912f666
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-15 22:56:57 +0900

    UserFindResponseDto 추가

---
+++ src/main/java/org/board/springboot/user/dto/UserFindResponseDto.java
@@ -0,0 +1,18 @@
+package org.board.springboot.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.board.springboot.user.domain.User;
+
+@Getter
+@NoArgsConstructor
+public class UserFindResponseDto {
+    private String name;
+    private String email;
+
+    public UserFindResponseDto(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
