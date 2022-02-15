f54088be059a5775ed7359e9652ceae6a1143d2e
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-16 00:20:19 +0900

    유저탐색 호출 성공 테스트 추가

--- src/test/java/org/board/springboot/user/UserServiceTest.java
+++ src/test/java/org/board/springboot/user/UserServiceTest.java
@@ -1,6 +1,8 @@
 package org.board.springboot.user;
 
+import org.board.springboot.user.domain.User;
 import org.board.springboot.user.domain.UserRepository;
+import org.board.springboot.user.dto.UserFindRequestDto;
 import org.board.springboot.user.dto.UserSaveRequestDto;
 import org.board.springboot.user.service.UserService;
 import org.junit.Test;
@@ -8,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -34,5 +38,24 @@ public class UserServiceTest {
 
         //then
         then(userRepository).should().save(any());
+    }
+
+    @Test
+    public void 유저탐색_호출_성공() {
+        //given
+        String email = "jk@jk.com";
+        String password = "jkjk";
+        UserFindRequestDto userFindRequestDto = UserFindRequestDto.builder()
+                .email(email)
+                .password(password)
+                .build();
+        given(userRepository.findByEmailAndPassword(email, password))
+                .willReturn(Optional.of(User.builder().build()));
+
+        //when
+        userService.find(userFindRequestDto);
+
+        //then
+        then(userRepository).should().findByEmailAndPassword(email, password);
     }
 }
