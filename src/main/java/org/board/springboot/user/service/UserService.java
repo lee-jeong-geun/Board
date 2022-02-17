3c9f1a62220616efb78549066757acdbeb2a166e
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-17 23:57:02 +0900

    유저 탐색 추가 Transactional annotation 추가

--- src/main/java/org/board/springboot/user/service/UserService.java
+++ src/main/java/org/board/springboot/user/service/UserService.java
@@ -1,9 +1,13 @@
 package org.board.springboot.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.board.springboot.user.domain.User;
 import org.board.springboot.user.domain.UserRepository;
+import org.board.springboot.user.dto.UserFindRequestDto;
+import org.board.springboot.user.dto.UserFindResponseDto;
 import org.board.springboot.user.dto.UserSaveRequestDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +15,15 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public Long save(UserSaveRequestDto userSaveRequestDto) {
         return userRepository.save(userSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public UserFindResponseDto find(UserFindRequestDto userFindRequestDto) {
+        User user = userRepository.findByEmailAndPassword(userFindRequestDto.getEmail(), userFindRequestDto.getPassword())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+        return new UserFindResponseDto(user);
     }
 }
