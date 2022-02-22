9517f31dba5d7cf2c8eff1f307f0b49584187629
ijeong-geun <lee-jeong-geun>
2022-02-23 01:20:17 +0900

    AuthServiceTest 추가 getLoginUserResponseDto 호출 테스트 추가

---
+++ src/test/java/org/board/springboot/auth/AuthServiceTest.java
@@ -0,0 +1,43 @@
+package org.board.springboot.auth;
+
+import org.board.springboot.auth.dto.LoginUserResponseDto;
+import org.board.springboot.auth.service.AuthService;
+import org.board.springboot.user.domain.User;
+import org.board.springboot.user.dto.UserFindResponseDto;
+import org.board.springboot.user.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthServiceTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    public void getLoginUserResponseDto_호출_올바른값_반환() {
+        //given
+        String name = "jk";
+        String email = "jk@jk.com";
+        User user = User.builder()
+                .name(name)
+                .email(email)
+                .build();
+        UserFindResponseDto userFindResponseDto = new UserFindResponseDto(user);
+
+        //when
+        LoginUserResponseDto loginUserResponseDto = authService.getLoginUserResponseDto(userFindResponseDto);
+
+        //then
+        then(loginUserResponseDto.getName()).isEqualTo(name);
+        then(loginUserResponseDto.getEmail()).isEqualTo(email);
+    }
+}
