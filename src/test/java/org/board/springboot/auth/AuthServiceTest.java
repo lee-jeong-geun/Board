8c78a5f7461788b2f2d30de8a2f11a11c733b9f6
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-23 20:06:41 +0900

    getLoginUserResponseDto_호출_올바른값_반환 테스트 삭제 getLoginUserResponseDto_호출_null값_반환 테스트 삭제

--- src/test/java/org/board/springboot/auth/AuthServiceTest.java
+++ src/test/java/org/board/springboot/auth/AuthServiceTest.java
@@ -2,7 +2,6 @@ package org.board.springboot.auth;
 
 import org.board.springboot.auth.dto.LoginRequestDto;
 import org.board.springboot.auth.dto.LoginResponseDto;
-import org.board.springboot.auth.dto.LoginUserResponseDto;
 import org.board.springboot.auth.service.AuthService;
 import org.board.springboot.user.domain.User;
 import org.board.springboot.user.dto.UserFindResponseDto;
@@ -27,36 +26,6 @@ public class AuthServiceTest {
 
     @InjectMocks
     private AuthService authService;
-
-    @Test
-    public void getLoginUserResponseDto_호출_올바른값_반환() {
-        //given
-        String name = "jk";
-        String email = "jk@jk.com";
-        User user = User.builder()
-                .name(name)
-                .email(email)
-                .build();
-        UserFindResponseDto userFindResponseDto = new UserFindResponseDto(user);
-
-        //when
-        LoginUserResponseDto loginUserResponseDto = authService.getLoginUserResponseDto(userFindResponseDto);
-
-        //then
-        then(loginUserResponseDto.getName()).isEqualTo(name);
-        then(loginUserResponseDto.getEmail()).isEqualTo(email);
-    }
-
-    @Test
-    public void getLoginUserResponseDto_호출_null값_반환() {
-        //given
-
-        //when
-        LoginUserResponseDto loginUserResponseDto = authService.getLoginUserResponseDto(null);
-
-        //then
-        then(loginUserResponseDto).isNull();
-    }
 
     @Test
     public void login_userService_호출_성공() {
