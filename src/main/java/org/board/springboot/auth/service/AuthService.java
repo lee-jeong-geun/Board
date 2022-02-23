ad94395539c7b4d059f3b310f66cc71174830618
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-23 20:21:20 +0900

    getLoginUserResponseDto 삭제 login 메서드 수정

--- src/main/java/org/board/springboot/auth/service/AuthService.java
+++ src/main/java/org/board/springboot/auth/service/AuthService.java
@@ -2,7 +2,6 @@ package org.board.springboot.auth.service;
 
 import lombok.RequiredArgsConstructor;
 import org.board.springboot.auth.dto.LoginRequestDto;
-import org.board.springboot.auth.dto.LoginResponseDto;
 import org.board.springboot.auth.dto.LoginUserResponseDto;
 import org.board.springboot.user.dto.UserFindResponseDto;
 import org.board.springboot.user.service.UserService;
@@ -14,24 +13,11 @@ public class AuthService {
 
     private final UserService userService;
 
-    public LoginUserResponseDto getLoginUserResponseDto(UserFindResponseDto userFindResponseDto) {
-        if (userFindResponseDto == null) {
-            return null;
-        }
+    public LoginUserResponseDto login(LoginRequestDto loginRequestDto) {
+        UserFindResponseDto userFindResponseDto = userService.find(loginRequestDto.toUserFindRequestDto());
         return LoginUserResponseDto.builder()
                 .name(userFindResponseDto.getName())
                 .email(userFindResponseDto.getEmail())
-                .build();
-    }
-
-    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
-        UserFindResponseDto userFindResponseDto = userService.find(loginRequestDto.toUserFindRequestDto());
-        boolean success = userFindResponseDto != null;
-        LoginUserResponseDto loginUserResponseDto = getLoginUserResponseDto(userFindResponseDto);
-
-        return LoginResponseDto.builder()
-                .success(success)
-                .user(loginUserResponseDto)
                 .build();
     }
 }
