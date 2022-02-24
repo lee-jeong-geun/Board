8fb8e774c31b16bae84a2ed5f86bf7b815b9fa61
lee-jeong-geun <ljk0411jg@naver.com>
2022-02-24 21:26:18 +0900

    ExceptionResponse 추가

---
+++ src/main/java/org/board/springboot/auth/dto/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package org.board.springboot.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ExceptionResponse {
+    private boolean success;
+    private String message;
+
+    @Builder
+    public ExceptionResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+}
