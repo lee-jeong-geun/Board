eba3465400b5e7e8cf688596aa8cc099dc56a1fa
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-01 19:39:34 +0900

    register.js 추가

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,23 @@
+(() => {
+    document.querySelector("#submit").addEventListener('click', () => {
+        fetch('/api/v1/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                password: document.getElementById('password').value
+            })
+        }).then((response) => {
+            response.json().then((body) => {
+                if (body.success === true) {
+                    alert('회원가입 성공하셨습니다.')
+                } else {
+                    alert(body.message)
+                }
+            })
+        }).catch(error => console.log(error))
+    })
+})()
