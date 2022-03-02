1ed26e01f2e11cc18feba5181bb92002ff6724bf
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-02 20:28:14 +0900

    logout 기능 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,18 +1,37 @@
 (() => {
-    document.querySelector("#login").addEventListener('click', () => {
-        fetch('/api/v1/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: document.getElementById('email').value,
-                password: document.getElementById('password').value
-            })
-        }).then((response) => {
+    document.querySelector("#login").addEventListener('click', (e) => {
+        const state = e.target.textContent;
+        let url
+        let init
+        if (state === '로그인') {
+            url = '/api/v1/auth/login'
+            init = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: document.getElementById('email').value,
+                    password: document.getElementById('password').value
+                })
+            }
+        } else {
+            url = '/api/v1/auth/logout'
+            init = {
+                method: 'POST'
+            }
+        }
+
+        fetch(url, init).then((response) => {
             response.json().then((body) => {
                 if (body.success === true) {
-                    alert('로그인에 성공하셨습니다.')
+                    if (state === '로그인') {
+                        alert('로그인에 성공하셨습니다.')
+                        e.target.textContent = '로그아웃'
+                    } else {
+                        alert('로그아웃에 성공하셨습니다.')
+                        e.target.textContent = '로그인'
+                    }
                 } else {
                     alert(body.message)
                 }
