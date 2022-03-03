4398dfb2be26e620c202ec526fe4abd3dbcfcf24
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-03 20:10:47 +0900

    logout 추가

--- src/main/java/org/board/springboot/auth/service/AuthService.java
+++ src/main/java/org/board/springboot/auth/service/AuthService.java
@@ -7,6 +7,8 @@ import org.board.springboot.user.dto.UserFindResponseDto;
 import org.board.springboot.user.service.UserService;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Service
 public class AuthService {
@@ -19,5 +21,14 @@ public class AuthService {
                 .name(userFindResponseDto.getName())
                 .email(userFindResponseDto.getEmail())
                 .build();
+    }
+
+
+    public boolean logout(HttpSession httpSession) {
+        if (httpSession.getAttribute("login") == null || !(boolean) httpSession.getAttribute("login")) {
+            throw new IllegalArgumentException("로그인 상태가 아닙니다.");
+        }
+        httpSession.setAttribute("login", false);
+        return true;
     }
 }
