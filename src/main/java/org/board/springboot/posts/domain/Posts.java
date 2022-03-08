130223b4a04f5ac8f6abf96b3ae72452f87433b6
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-08 23:42:59 +0900

    Posts 추가

---
+++ src/main/java/org/board/springboot/posts/domain/Posts.java
@@ -0,0 +1,35 @@
+package org.board.springboot.posts.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.board.springboot.user.domain.User;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Builder
+    public Posts(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
