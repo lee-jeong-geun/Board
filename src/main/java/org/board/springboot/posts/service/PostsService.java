47088ec2fe29638bd80a643e4c05797e46217d8b
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-11 01:52:45 +0900

    findAll 추가

--- src/main/java/org/board/springboot/posts/service/PostsService.java
+++ src/main/java/org/board/springboot/posts/service/PostsService.java
@@ -2,9 +2,13 @@ package org.board.springboot.posts.service;
 
 import lombok.RequiredArgsConstructor;
 import org.board.springboot.posts.domain.PostsRepository;
+import org.board.springboot.posts.dto.PostsFindResponseDto;
 import org.board.springboot.posts.dto.PostsSaveRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +19,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto postsSaveRequestDto) {
         return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsFindResponseDto> findAll() {
+        return postsRepository.findAll()
+                .stream()
+                .map(p -> PostsFindResponseDto.builder()
+                        .title(p.getTitle())
+                        .content(p.getContent())
+                        .userName(p.getUser().getName())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
