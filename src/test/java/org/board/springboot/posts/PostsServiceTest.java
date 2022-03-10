802eae2520d9c380400432fb1444b12586b32d91
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-11 01:51:21 +0900

    포스트 전체 검색 호출 성공 테스트 추가

--- src/test/java/org/board/springboot/posts/PostsServiceTest.java
+++ src/test/java/org/board/springboot/posts/PostsServiceTest.java
@@ -3,8 +3,10 @@ package org.board.springboot.posts;
 import org.assertj.core.api.BDDAssertions;
 import org.board.springboot.posts.domain.Posts;
 import org.board.springboot.posts.domain.PostsRepository;
+import org.board.springboot.posts.dto.PostsFindResponseDto;
 import org.board.springboot.posts.dto.PostsSaveRequestDto;
 import org.board.springboot.posts.service.PostsService;
+import org.board.springboot.user.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,6 +14,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -44,5 +49,39 @@ public class PostsServiceTest {
         //then
         then(postsRepository).should(times(1)).save(any());
         BDDAssertions.then(id).isEqualTo(1l);
+    }
+
+    @Test
+    public void 포스트_전체_검색_호출_성공() {
+        //given
+        String name = "jk";
+        User user = User.builder()
+                .name(name)
+                .build();
+
+        Posts posts1 = Posts.builder()
+                .title("title1")
+                .content("content1")
+                .user(user)
+                .build();
+
+        Posts posts2 = Posts.builder()
+                .user(user)
+                .build();
+        List<Posts> list = new ArrayList(Arrays.asList(new Posts[]{posts1, posts2}));
+
+        given(postsRepository.findAll()).willReturn(list);
+
+
+        //when
+        List<PostsFindResponseDto> result = postsService.findAll();
+
+        //then
+        then(postsRepository).should(times(1)).findAll();
+        BDDAssertions.then(result.size()).isEqualTo(2);
+        BDDAssertions.then(result.get(0).getTitle()).isEqualTo("title1");
+        BDDAssertions.then(result.get(0).getContent()).isEqualTo("content1");
+        BDDAssertions.then(result.get(0).getUserName()).isEqualTo(name);
+        BDDAssertions.then(result.get(1).getUserName()).isEqualTo(name);
     }
 }
