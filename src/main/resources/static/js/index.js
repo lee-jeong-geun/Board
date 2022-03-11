20e06c6deb4c0fb1846102532eb168b90a3e596c
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-12 01:03:45 +0900

    게시글 리스트 로드 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -38,4 +38,39 @@
             })
         }).catch((error) => console.log(error))
     })
+
+    const setPostsList = (list) => {
+        const tbody = document.getElementById('posts-list')
+        list.forEach((element, index) => {
+            const node = document.createElement('tr')
+            let childNode = document.createElement('th')
+            childNode.innerText = index + 1;
+            node.appendChild(childNode);
+
+            childNode = document.createElement('th')
+            childNode.innerText = element.title;
+            node.appendChild(childNode);
+
+            childNode = document.createElement('th')
+            childNode.innerText = element.userName;
+            node.appendChild(childNode);
+
+            tbody.appendChild(node);
+        })
+    }
+
+    const loadData = () => {
+        fetch('/api/v1/posts', {
+            method: 'GET'
+        }).then((response) => {
+            response.json().then((body) => {
+                if (body.success === true) {
+                    setPostsList(body.postsList)
+                } else {
+                    console.log('조회에 실패하였습니다.')
+                }
+            })
+        }).catch((error) => console.log(error))
+    }
+    loadData()
 })()
