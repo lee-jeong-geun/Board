e3d26d19fb6ee3b993563e71bf793627851d461a
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-17 21:20:47 +0900

    findPostsById 추가 IllegalArgumentExceptionHandler 추가

--- src/main/java/org/board/springboot/user/controller/UserApiController.java
+++ src/main/java/org/board/springboot/user/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package org.board.springboot.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.board.springboot.user.dto.UserFindPostsListResponseDto;
 import org.board.springboot.user.dto.UserSaveRequestDto;
 import org.board.springboot.user.service.UserService;
+import org.board.springboot.common.dto.ApiResponse;
+import org.board.springboot.common.dto.ExceptionResponse;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,5 +19,21 @@ public class UserApiController {
     @PostMapping("/api/v1/users")
     public Long save(@RequestBody UserSaveRequestDto requestDto) {
         return userService.save(requestDto);
+    }
+
+    @GetMapping("/api/v1/users/{id}")
+    public ApiResponse<List<UserFindPostsListResponseDto>> findPostsById(@PathVariable Long id) {
+        return ApiResponse.<List<UserFindPostsListResponseDto>>builder()
+                .success(true)
+                .response(userService.findPostsById(id))
+                .build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ExceptionResponse IllegalArgumentExceptionHandler(Exception exception) {
+        return ExceptionResponse.builder()
+                .success(false)
+                .message(exception.getMessage())
+                .build();
     }
 }
