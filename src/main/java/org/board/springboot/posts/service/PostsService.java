63c0bc568d3cccace971b1261d2fd31a9fd1dd69
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-20 18:12:13 +0900

    findAll userId, userName 삭제 userEmail 추가

--- src/main/java/org/board/springboot/posts/service/PostsService.java
+++ src/main/java/org/board/springboot/posts/service/PostsService.java
@@ -38,8 +38,7 @@ public class PostsService {
                 .map(p -> PostsFindResponseDto.builder()
                         .title(p.getTitle())
                         .content(p.getContent())
-                        .userId(p.getUser().getId())
-                        .userName(p.getUser().getName())
+                        .userEmail(p.getUser().getEmail())
                         .build())
                 .collect(Collectors.toList());
     }
