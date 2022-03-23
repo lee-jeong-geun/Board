caaee61df9d5496a55db58d3b8604a3d3d8ff1ad
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-24 01:41:12 +0900

    init 추가

--- src/main/resources/static/js/posts/create.js
+++ src/main/resources/static/js/posts/create.js
@@ -20,4 +20,28 @@
             })
         }).catch((error) => console.log(error))
     })
+
+    const init = () => {
+         const loggedInCheck = body => {
+            if (body.success) {
+                if (!body.response) {
+                    alert('로그인 상태가 아닙니다')
+                    window.location.href = '/'
+                }
+            } else {
+                alert(body.message)
+            }
+        }
+
+        const url = '/api/v1/auth/logged-in'
+        const init = {
+            method: 'GET'
+        }
+        fetch(url, init).then(response => {
+            response.json().then(body => {
+                loggedInCheck(body)
+            })
+        }).catch(error => console.log(error))
+    }
+    init()
 })()
