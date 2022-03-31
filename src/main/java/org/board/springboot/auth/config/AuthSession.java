f1734e8c2fd222b6d651ac0f5f66e42f845cc089
lee-jeong-geun <ljk0411jg@naver.com>
2022-03-31 14:16:17 +0900

    currentHashMap -> redis 수정

--- src/main/java/org/board/springboot/auth/config/AuthSession.java
+++ src/main/java/org/board/springboot/auth/config/AuthSession.java
@@ -2,11 +2,18 @@ package org.board.springboot.auth.config;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.Map;
 
-@RequiredArgsConstructor
 @Getter
 public class AuthSession {
-    private final Map<String, Object> session;
+    private final HashOperations<String, String, Object> session;
+
+    @Autowired
+    public AuthSession(RedisTemplate redisTemplate) {
+        this.session = redisTemplate.opsForHash();
+    }
 }
