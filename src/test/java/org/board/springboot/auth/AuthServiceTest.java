54da8b4e403f0904aa6976674b4f53590bfb30fd
lee-jeong-geun <ljk0411jg@naver.com>
2022-04-12 17:08:56 +0900

    리팩토링 redisTemplate, map 삭제 UserSessionService 추가 login_userService_호출_성공, logout_호출_성공, login_이메일_로그인_상태_호출_실패_에러 테스트 수정

--- src/test/java/org/board/springboot/auth/AuthServiceTest.java
+++ src/test/java/org/board/springboot/auth/AuthServiceTest.java
@@ -6,14 +6,13 @@ import org.board.springboot.auth.service.AuthService;
 import org.board.springboot.user.domain.User;
 import org.board.springboot.user.dto.UserFindResponseDto;
 import org.board.springboot.user.service.UserService;
+import org.board.springboot.user.service.UserSessionService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mock.web.MockHttpSession;
 
 import java.util.Optional;
@@ -21,6 +20,7 @@ import java.util.Optional;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,22 +30,20 @@ public class AuthServiceTest {
     private UserService userService;
 
     @Mock
+    private UserSessionService userSessionService;
+
+    @Mock
     private MockHttpSession mockHttpSession;
-
-    @Mock
-    private HashOperations<String, Object, Object> map;
-
-    @Mock
-    private RedisTemplate<String, Object> redisTemplate;
 
     @InjectMocks
     private AuthService authService;
+
+    private final String email = "jk@jk.com";
 
     @Test
     public void login_userService_호출_성공() {
         //given
         String name = "jk";
-        String email = "jk@jk.com";
         LoginRequestDto loginRequestDto = LoginRequestDto.builder()
                 .email(email)
                 .build();
@@ -53,21 +51,18 @@ public class AuthServiceTest {
                 .name(name)
                 .email(email)
                 .build());
-        given(userService.findByEmailAndPassword(any())).willReturn(userFindResponseDto);
-        given(redisTemplate.opsForHash()).willReturn(map);
-        given(map.hasKey(email, "login")).willReturn(false);
         given(mockHttpSession.getAttribute("login")).willReturn(null);
+        given(userService.findByEmailAndPassword(any())).willReturn(userFindResponseDto);
 
         //when
         LoginUserResponseDto result = authService.login(loginRequestDto, mockHttpSession);
 
         //then
-        BDDMockito.then(userService).should().findByEmailAndPassword(any());
-        BDDMockito.then(mockHttpSession).should().setAttribute("login", email);
         BDDMockito.then(mockHttpSession).should().getAttribute("login");
-        BDDMockito.then(redisTemplate).should(times(2)).opsForHash();
-        BDDMockito.then(map).should().put(email, "login", "true");
-        BDDMockito.then(map).should().hasKey(email, "login");
+        BDDMockito.then(userService).should().findByEmailAndPassword(any());
+        BDDMockito.then(userSessionService).should().validateLoginEmailState(email);
+        BDDMockito.then(userSessionService).should().createLoginState(email);
+        BDDMockito.then(mockHttpSession).should().setAttribute("login", email);
         then(result.getName()).isEqualTo(name);
         then(result.getEmail()).isEqualTo(email);
     }
@@ -75,22 +70,20 @@ public class AuthServiceTest {
     @Test
     public void logout_호출_성공() {
         //given
-        given(redisTemplate.opsForHash()).willReturn(map);
         given(mockHttpSession.getAttribute("login")).willReturn(true);
 
         //when
         boolean result = authService.logout(mockHttpSession);
 
         //then
-        BDDMockito.then(redisTemplate).should().opsForHash();
         BDDMockito.then(mockHttpSession).should(times(2)).getAttribute("login");
-        BDDMockito.then(map).should().delete(mockHttpSession.getAttribute("login").toString(), "login");
+        BDDMockito.then(userSessionService).should().deleteLoginState(mockHttpSession.getAttribute("login").toString());
         BDDMockito.then(mockHttpSession).should().removeAttribute("login");
         then(result).isEqualTo(true);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void logout_세션_null값_호출_실패_에러() {
+    public void logout_세션_null_값_호출_실패_에러() {
         //given
         given(mockHttpSession.getAttribute("login")).willReturn(null);
 
@@ -109,7 +102,7 @@ public class AuthServiceTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void login_세션_not_null값_호출_실패_에러() {
+    public void login_세션_not_null_값_호출_실패_에러() {
         //given
         LoginRequestDto loginRequestDto = LoginRequestDto.builder().build();
         given(mockHttpSession.getAttribute("login")).willReturn(Optional.empty());
@@ -121,13 +114,11 @@ public class AuthServiceTest {
     @Test(expected = IllegalArgumentException.class)
     public void login_이메일_로그인_상태_호출_실패_에러() {
         //given
-        String email = "jk@jk.com";
         LoginRequestDto loginRequestDto = LoginRequestDto.builder()
                 .email(email)
                 .build();
         given(userService.findByEmailAndPassword(any())).willReturn(new UserFindResponseDto(User.builder().build()));
-        given(redisTemplate.opsForHash()).willReturn(map);
-        given(map.hasKey(email, "login")).willReturn(true);
+        willThrow(new IllegalArgumentException()).given(userSessionService).validateLoginEmailState(email);
 
         //when
         authService.login(loginRequestDto, mockHttpSession);
