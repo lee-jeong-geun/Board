4173a83bbd8db3b666d1ad9ebac59a295b3295c1
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-04 16:56:40 +0900

    viewCount 추가

--- src/main/java/org/board/springboot/posts/dto/PostsFindResponseDto.java
+++ src/main/java/org/board/springboot/posts/dto/PostsFindResponseDto.java
@@ -12,13 +12,15 @@ public class PostsFindResponseDto {
     private Long postsId;
     private String title;
     private String content;
+    private int viewCount;
     private String userEmail;
 
     @Builder
-    public PostsFindResponseDto(Long postsId, String title, String content, String userEmail) {
+    public PostsFindResponseDto(Long postsId, String title, String content, int viewCount, String userEmail) {
         this.postsId = postsId;
         this.title = title;
         this.content = content;
+        this.viewCount = viewCount;
         this.userEmail = userEmail;
     }
 }
