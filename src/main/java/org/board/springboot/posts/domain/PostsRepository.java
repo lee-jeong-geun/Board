fd3f1900d1e0151dc98de4ba0731e17a58e5b540
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-05 19:29:57 +0900

    findByIdForUpdate 추가

--- src/main/java/org/board/springboot/posts/domain/PostsRepository.java
+++ src/main/java/org/board/springboot/posts/domain/PostsRepository.java
@@ -1,6 +1,16 @@
 package org.board.springboot.posts.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from posts p where p.id = :id")
+    Optional<Posts> findByIdForUpdate(@Param("id") Long id);
 }
