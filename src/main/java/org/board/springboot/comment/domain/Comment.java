dce0f40a48536862ea48e1fd17cffcf7a622d335
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-09 18:47:23 +0900

    Comment 추가

---
+++ src/main/java/org/board/springboot/comment/domain/Comment.java
@@ -0,0 +1,39 @@
+package org.board.springboot.comment.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.board.springboot.posts.domain.Posts;
+import org.board.springboot.user.domain.User;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long Id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "posts_id")
+    private Posts posts;
+
+    @Builder
+    public Comment(String content, User user, Posts posts) {
+        this.content = content;
+        this.user = user;
+        this.posts = posts;
+        user.getCommentList().add(this);
+        posts.getCommentList().add(this);
+    }
+}
