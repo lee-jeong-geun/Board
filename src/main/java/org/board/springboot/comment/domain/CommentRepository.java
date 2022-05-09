83f431958fb89f1ccca2319b517e659ff66a2ed3
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-09 18:47:44 +0900

    CommentRepository 추가

---
+++ src/main/java/org/board/springboot/comment/domain/CommentRepository.java
@@ -0,0 +1,8 @@
+package org.board.springboot.comment.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
