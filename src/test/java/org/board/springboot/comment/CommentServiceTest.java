85f9f73c92f2ca02d985443f6c0c9d702956047c
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-12 01:41:25 +0900

    CommentServiceTest 추가 save_호출_성공 테스트 추가

---
+++ src/test/java/org/board/springboot/comment/CommentServiceTest.java
@@ -0,0 +1,74 @@
+package org.board.springboot.comment;
+
+import org.board.springboot.comment.domain.Comment;
+import org.board.springboot.comment.domain.CommentRepository;
+import org.board.springboot.comment.dto.CommentSaveRequestDto;
+import org.board.springboot.comment.service.CommentService;
+import org.board.springboot.posts.domain.Posts;
+import org.board.springboot.posts.domain.PostsRepository;
+import org.board.springboot.user.domain.User;
+import org.board.springboot.user.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommentServiceTest {
+
+    @InjectMocks
+    private CommentService commentService;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private PostsRepository postsRepository;
+
+    @Test
+    public void save_호출_성공() throws Exception {
+        //given
+        String content = "content";
+        String userEmail = "jk@jk.com";
+        Long postsId = 1l;
+        User user = User.builder().build();
+        Posts posts = Posts.builder()
+                .user(user)
+                .build();
+        Comment comment = Comment.builder()
+                .content(content)
+                .user(user)
+                .posts(posts)
+                .build();
+        Field field = comment.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(comment, 1l);
+
+        CommentSaveRequestDto commentSaveRequestDto = CommentSaveRequestDto.builder()
+                .content(content)
+                .userEmail(userEmail)
+                .postsId(postsId)
+                .build();
+        given(userService.findByEmail(userEmail)).willReturn(user);
+        given(postsRepository.findById(postsId)).willReturn(Optional.of(posts));
+        given(commentRepository.save(any())).willReturn(comment);
+
+        //when
+        Long result = commentService.save(commentSaveRequestDto);
+
+        //then
+        then(userService).should().findByEmail(userEmail);
+        then(postsRepository).should().findById(postsId);
+        then(commentRepository).should().save(any());
+        assertThat(result).isEqualTo(1l);
+    }
+}
