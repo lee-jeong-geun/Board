07b18e4da8cf96b27fb57a02800b7cb0ecf0d4fe
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-17 22:29:23 +0900

    Comment_save_호출_실패_잘못된_userEmail_에러처리 테스트 추가

--- src/test/java/org/board/springboot/comment/CommentApiControllerTest.java
+++ src/test/java/org/board/springboot/comment/CommentApiControllerTest.java
@@ -7,6 +7,7 @@ import org.board.springboot.comment.controller.CommentApiController;
 import org.board.springboot.comment.dto.CommentSaveRequestDto;
 import org.board.springboot.comment.service.CommentService;
 import org.board.springboot.common.dto.ApiResponse;
+import org.board.springboot.common.dto.ExceptionResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -17,12 +18,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -66,10 +67,41 @@ public class CommentApiControllerTest {
 
         //then
         resultActions.andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(apiResponse)));
+                .andExpect(content().string(objectMapper.writeValueAsString(apiResponse)));
         then(commentService).should().save(argumentCaptor.capture());
         BDDAssertions.then(argumentCaptor.getValue().getContent()).isEqualTo(content);
         BDDAssertions.then(argumentCaptor.getValue().getUserEmail()).isEqualTo(userEmail);
         BDDAssertions.then(argumentCaptor.getValue().getPostsId()).isEqualTo(postsId);
+    }
+
+    @Test
+    public void Comment_save_호출_실패_잘못된_userEmail_에러처리() throws Exception {
+        //given
+        String url = "/api/v1/comment";
+        String content = "content";
+        String userEmail = "jk@jk.com";
+        Long postsId = 1l;
+
+        CommentSaveRequestDto commentSaveRequestDto = CommentSaveRequestDto.builder()
+                .content(content)
+                .userEmail(userEmail)
+                .postsId(postsId)
+                .build();
+        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .success(false)
+                .message("해당 유저가 없습니다.")
+                .build();
+
+        given(commentService.save(any())).willThrow(new IllegalArgumentException("해당 유저가 없습니다."));
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(commentSaveRequestDto)));
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(exceptionResponse)));
+        then(commentService).should().save(any());
     }
 }
