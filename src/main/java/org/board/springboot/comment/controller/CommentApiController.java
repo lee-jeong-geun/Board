a3e3f9463224f6bd1a96e6073ce5b4525b5e472b
lee-jeong-geun <ljk0411jg@naver.com>
2022-05-22 23:42:22 +0900

    AuthService 추가 JWTService 추가 HttpServletRequest 추가
    commentSave 수정
    
    validateLoginState 추가

--- src/main/java/org/board/springboot/comment/controller/CommentApiController.java
+++ src/main/java/org/board/springboot/comment/controller/CommentApiController.java
@@ -1,6 +1,9 @@
 package org.board.springboot.comment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.board.springboot.auth.service.AuthService;
+import org.board.springboot.auth.service.JWTService;
+import org.board.springboot.comment.dto.CommentSaveRequestBody;
 import org.board.springboot.comment.dto.CommentSaveRequestDto;
 import org.board.springboot.comment.service.CommentService;
 import org.board.springboot.common.dto.ApiResponse;
@@ -10,14 +13,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.board.springboot.util.CookieUtils.getCookie;
+
 @RequiredArgsConstructor
 @RestController
 public class CommentApiController {
 
     private final CommentService commentService;
+    private final AuthService authService;
+    private final JWTService jwtService;
+    private final HttpServletRequest httpServletRequest;
 
     @PostMapping("/api/v1/comment")
-    public ApiResponse<Long> commentSave(@RequestBody CommentSaveRequestDto commentSaveRequestDto) {
+    public ApiResponse<Long> commentSave(@RequestBody CommentSaveRequestBody commentSaveRequestBody) {
+        validateLoginState();
+
+        Cookie tokenCookie = getCookie(httpServletRequest.getCookies(), "token");
+        String userEmail = jwtService.getEmail(tokenCookie.getValue());
+
+        CommentSaveRequestDto commentSaveRequestDto = CommentSaveRequestDto.builder()
+                .content(commentSaveRequestBody.getContent())
+                .userEmail(userEmail)
+                .postsId(commentSaveRequestBody.getPostsId())
+                .build();
         Long id = commentService.save(commentSaveRequestDto);
 
         return ApiResponse.<Long>builder()
@@ -32,5 +53,11 @@ public class CommentApiController {
                 .success(false)
                 .message(exception.getMessage())
                 .build();
+    }
+
+    private void validateLoginState() {
+        if (!authService.isLoggedIn()) {
+            throw new IllegalStateException("로그인 상태가 아닙니다.");
+        }
     }
 }
