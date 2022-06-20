ddc32e3ec15de888e34f5f7b710c14631df958d7
lee-jeong-geun <ljk0411jg@naver.com>
2022-06-20 21:50:53 +0900

    findByUserEmail 추가

--- src/main/java/org/board/springboot/auth/domain/AuthUserRepository.java
+++ src/main/java/org/board/springboot/auth/domain/AuthUserRepository.java
@@ -2,6 +2,8 @@ package org.board.springboot.auth.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthUserRepository extends JpaRepository<AuthUser, Long> {
-    
+    Optional<AuthUser> findByUserEmail(String email);
 }
