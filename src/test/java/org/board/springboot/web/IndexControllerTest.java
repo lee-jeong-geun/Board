4c972b4195dbc79f7482f5f9995e5d9bb69e136d
lee-jeong-geun <ljk0411jg@naver.com>
2022-08-02 22:51:18 +0900

    테스트 버전 수정

--- src/test/java/org/board/springboot/web/IndexControllerTest.java
+++ src/test/java/org/board/springboot/web/IndexControllerTest.java
@@ -1,18 +1,15 @@
 package org.board.springboot.web;
 
 import org.hamcrest.Matchers;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(IndexController.class)
 public class IndexControllerTest {
 
@@ -20,7 +17,7 @@ public class IndexControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void 메인화면_로딩_성공() throws Exception {
+    void 메인화면_로딩_성공() throws Exception {
         //given
         String url = "http://localhost:8080/";
 
@@ -33,7 +30,7 @@ public class IndexControllerTest {
     }
 
     @Test
-    public void 회원가입화면_로딩_성공() throws Exception {
+    void 회원가입화면_로딩_성공() throws Exception {
         //given
         String url = "http://localhost:8080/auth/register";
 
