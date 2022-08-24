ca2ea782658ce1d067a425101ce8a2a8881f18d5
lee-jeong-geun <ljk0411jg@naver.com>
2022-08-25 01:24:30 +0900

    edit click event 추가

--- src/main/resources/static/js/user/edit.js
+++ src/main/resources/static/js/user/edit.js
@@ -1,16 +1,38 @@
 (() => {
     document.getElementById('edit').addEventListener('click', () => {
+        const urlArray = window.location.href.split("/")
+        const email = urlArray[urlArray.length - 2]
+        const url = '/api/v1/users/' + email
 
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: document.getElementById('name').value,
+                password: document.getElementById('password').value
+            })
+        }).then((response) => {
+            response.json().then((body) => {
+                if (body.success === true) {
+                    alert('업데이트 성공하셨습니다.')
+                    location.reload()
+                } else {
+                    alert(body.message)
+                }
+            })
+        }).catch(error => console.log(error))
     })
 
     const setInform = (response) => {
         const nameElement = document.getElementById('name')
-        nameElement.innerText = response.name
+        nameElement.value = response.name
     }
 
     const loadData = () => {
-        const urlArray = window.location.href.split("/")
-        const email = urlArray[urlArray.length - 1]
+        const urlArray = window.location.href.split('/')
+        const email = urlArray[urlArray.length - 2]
         const url = '/api/v1/users/' + email
 
         fetch(url, {
