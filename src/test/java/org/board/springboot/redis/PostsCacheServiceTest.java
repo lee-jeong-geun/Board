d72812ae926cfad54328e3d719d3d52958aeede7
lee-jeong-geun <ljk0411jg@naver.com>
2022-10-09 22:21:15 +0900

    파일 추가

---
+++ src/test/java/org/board/springboot/redis/PostsCacheServiceTest.java
@@ -0,0 +1,96 @@
+package org.board.springboot.redis;
+
+import org.board.springboot.redis.posts.PostsCacheService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+public class PostsCacheServiceTest {
+
+    @Mock
+    RedisTemplate<String, Object> redisTemplate;
+    @Mock
+    ValueOperations<String, Object> valueOperations;
+
+    @InjectMocks
+    PostsCacheService postsCacheService;
+
+    String VIEW_COUNT = "postsViewCount";
+
+    @Test
+    void hasPostsViewCountKey_호출_성공_키_존재() {
+        //given
+        Long postsId = 1L;
+        String key = VIEW_COUNT + ":" + postsId;
+
+        given(redisTemplate.hasKey(key)).willReturn(true);
+
+        //when
+        boolean result = postsCacheService.hasPostsViewCountKey(postsId);
+
+        //then
+        assertTrue(result);
+        then(redisTemplate).should().hasKey(key);
+    }
+
+    @Test
+    void hasPostsViewCountKey_호출_성공_키_미존재() {
+        //given
+        Long postsId = 1L;
+        String key = VIEW_COUNT + ":" + postsId;
+
+        given(redisTemplate.hasKey(key)).willReturn(false);
+
+        //when
+        boolean result = postsCacheService.hasPostsViewCountKey(postsId);
+
+        //then
+        assertFalse(result);
+        then(redisTemplate).should().hasKey(key);
+    }
+
+    @Test
+    void setPostsViewCount_호출_성공() {
+        //given
+        Long postsId = 1L;
+        int viewCount = 0;
+        String key = VIEW_COUNT + ":" + postsId;
+        Object value = viewCount;
+
+        given(redisTemplate.opsForValue()).willReturn(valueOperations);
+
+        //when
+        postsCacheService.setPostsViewCount(postsId, viewCount);
+
+        //then
+        then(redisTemplate).should().opsForValue();
+        then(valueOperations).should().set(key, value);
+    }
+
+    @Test
+    void incrementPostsViewCount_호출_성공() {
+        //given
+        Long postsId = 1L;
+        int updateCount = 1;
+        String key = VIEW_COUNT + ":" + postsId;
+
+        given(redisTemplate.opsForValue()).willReturn(valueOperations);
+
+        //when
+        postsCacheService.incrementPostsViewCount(postsId, updateCount);
+
+        //then
+        then(redisTemplate).should().opsForValue();
+        then(valueOperations).should().increment(key, updateCount);
+    }
+}
