87bfd57f7337ad011325ca574af45796d805666a
lee-jeong-geun <ljk0411jg@naver.com>
2022-10-09 22:41:31 +0900

    파일 추가

---
+++ src/main/java/org/board/springboot/redis/posts/PostsCacheService.java
@@ -0,0 +1,33 @@
+package org.board.springboot.redis.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostsCacheService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final String VIEW_COUNT = "postsViewCount";
+
+    public boolean hasPostsViewCountKey(Long postsId) {
+        String key = getPostsViewCount(postsId);
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void setPostsViewCount(Long postsId, int viewCount) {
+        String key = getPostsViewCount(postsId);
+        Object value = String.valueOf(viewCount);
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void incrementPostsViewCount(Long postsId, int updateCount) {
+        String key = getPostsViewCount(postsId);
+        redisTemplate.opsForValue().increment(key, updateCount);
+    }
+
+    private String getPostsViewCount(Long postsId) {
+        return VIEW_COUNT + ":" + postsId;
+    }
+}
